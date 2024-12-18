8e503688d031aaa82f32e264d897c5a1c76b50b4
Kevin Ngari <122980206+ngarikev@users.noreply.github.com>
2024-12-18 13:38:02 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,11 +4,15 @@
 // init project
 var express = require('express');
 var app = express();
-
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+var corsOptions = {
+  origin: "https://en.wikipedia.org/wiki/Cross-origin_resource_sharing",
+  optionsSuccessStatus: 200 // some legacy browsers choke on 204
+}
+// so that your API is remotely testable by FCC 
+
+app.use(cors(corsOptions));  
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
